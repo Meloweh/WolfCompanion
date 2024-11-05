7029a96d05b7c62d920f81c2fb4982d2313376f4
Meloweh <49780209+Meloweh@users.noreply.github.com>
2024-11-05 09:13:05 +0100

    done

--- src/client/java/renderer/CustomWolfEntityRenderer.java
+++ src/client/java/renderer/CustomWolfEntityRenderer.java
@@ -1,7 +1,7 @@
 package renderer;
 
-import github.meloweh.wolfcompanion.WolfCompanion;
-import github.meloweh.wolfcompanion.model.ExampleChestModel;
+//import github.meloweh.wolfcompanion.model.ExampleChestModel;
+import github.meloweh.wolfcompanion.model.WolfChestModel;
 import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.render.OverlayTexture;
 import net.minecraft.client.render.RenderLayer;
@@ -14,6 +14,11 @@ import net.minecraft.client.render.entity.feature.FeatureRendererContext;
 import net.minecraft.client.render.entity.model.WolfEntityModel;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.passive.WolfEntity;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
+import org.joml.Matrix4f;
+import org.joml.Quaternionf;
+import org.joml.Vector3f;
 
 public class CustomWolfEntityRenderer extends WolfEntityRenderer {
     public CustomWolfEntityRenderer(EntityRendererFactory.Context context) {
@@ -25,29 +30,73 @@ public class CustomWolfEntityRenderer extends WolfEntityRenderer {
 
     // Custom feature renderer class
     private static class CustomFeatureRenderer extends FeatureRenderer<WolfEntity, WolfEntityModel<WolfEntity>> {
-        private final ExampleChestModel chestModel;
+        private final WolfChestModel chestModel;
 
         public CustomFeatureRenderer(FeatureRendererContext<WolfEntity, WolfEntityModel<WolfEntity>> context) {
             super(context);
             //this.chestModel = new ExampleChestModel(context.getLayerModelPart(ExampleChestModel.LAYER_LOCATION));
-            final ModelPart chestRoot = ChestModelUtil.createChestRoot();
-            this.chestModel = new ExampleChestModel(chestRoot);
+            final ModelPart chestRoot = WolfChestModel.getTexturedModelData().createModel();//ChestModelUtil.createChestRoot();
+            this.chestModel = new WolfChestModel(chestRoot);
         }
+
+        /*
+        private void renderChest(Direction side, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, WolfEntity entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
+            final boolean isRightSide = side.equals(Direction.EAST);
+            final double rotationToSide = isRightSide ? 90 : 180;
+            final Vec3d translation = new Vec3d(isRightSide ? -0.3 : 0, 0.37, 0.3);
+
+            matrices.push();
+
+            matrices.translate(translation.getX(), translation.getY(), translation.getZ());
+            //matrices.scale(0.35f, 0.35f, 0.35f); // Scale to fit on the wolf
+
+            // Rotate to the wolf side
+            Quaternionf rotation = new Quaternionf().rotateY((float) Math.toRadians(rotationToSide));
+            matrices.multiply(rotation);
+
+            VertexConsumer vertexConsumer = vertexConsumers.getBuffer(chestModel.getRenderLayer());
+
+            // Render the chest model
+            chestModel.render(matrices, vertexConsumers.getBuffer(RenderLayer.getEntitySolid(WolfChestModel.TEXTURE_LOCATION)), light, OverlayTexture.DEFAULT_UV);
+
+            matrices.pop();
+        }
+
+        @Override
+        public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, WolfEntity entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
+            renderChest(Direction.EAST, matrices, vertexConsumers, light, entity, limbSwing, limbSwingAmount, partialTicks, ageInTicks, netHeadYaw, headPitch);
+            renderChest(Direction.WEST, matrices, vertexConsumers, light, entity, limbSwing, limbSwingAmount, partialTicks, ageInTicks, netHeadYaw, headPitch);
+        }*/
 
         @Override
         public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, WolfEntity entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
             matrices.push();
 
-            // Position and scale the chest model on the wolf’s back
-            matrices.translate(0.22, 0.8, 0.4); // Adjust as needed to center on wolf's back
-            matrices.scale(0.35f, 0.35f, 0.35f); // Scale to fit on the wolf
+            matrices.translate(-0.3, -0.37, 0.26);
+            //matrices.scale(0.35f, 0.35f, 0.35f); // Scale to fit on the wolf
 
-            // Bind texture if needed
-            VertexConsumer vertexConsumer = vertexConsumers.getBuffer(chestModel.getRenderLayer());
+            // Rotate to the wolf side
+            matrices.multiply(new Quaternionf().rotateY((float) Math.toRadians(90)));
+
+            //VertexConsumer vertexConsumer = vertexConsumers.getBuffer(chestModel.getRenderLayer());
 
             // Render the chest model
-            //chestModel.render(matrices, vertexConsumer, light, OverlayTexture.DEFAULT_UV);
-            chestModel.render(matrices, vertexConsumers.getBuffer(RenderLayer.getEntitySolid(ExampleChestModel.TEXTURE_LOCATION)), light, OverlayTexture.DEFAULT_UV);
+            chestModel.render(matrices, vertexConsumers.getBuffer(RenderLayer.getEntitySolid(WolfChestModel.TEXTURE_LOCATION)), light, OverlayTexture.DEFAULT_UV);
+
+            matrices.pop();
+
+            matrices.push();
+
+            matrices.translate(0.3, -0.37, 0.45);
+            //matrices.scale(0.35f, 0.35f, 0.35f); // Scale to fit on the wolf
+
+            // Rotate to the wolf side
+            matrices.multiply(new Quaternionf().rotateY((float) Math.toRadians(-90)));
+
+            //VertexConsumer vertexConsumerLeft = vertexConsumers.getBuffer(chestModel.getRenderLayer());
+
+            // Render the chest model
+            chestModel.render(matrices, vertexConsumers.getBuffer(RenderLayer.getEntitySolid(WolfChestModel.TEXTURE_LOCATION)), light, OverlayTexture.DEFAULT_UV);
 
             matrices.pop();
         }
