c8f0765632b36f324e67111d1a3261113fec3431
Meloweh <49780209+Meloweh@users.noreply.github.com>
2024-12-03 07:32:12 +0100

    f

--- src/client/java/renderer/WolfBagFeatureRenderer.java
+++ src/client/java/renderer/WolfBagFeatureRenderer.java
@@ -16,10 +16,12 @@ import net.minecraft.client.render.entity.state.FoxEntityRenderState;
 import net.minecraft.client.render.entity.state.WolfEntityRenderState;
 import net.minecraft.client.render.item.HeldItemRenderer;
 import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.passive.WolfEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ModelTransformationMode;
 import net.minecraft.util.math.RotationAxis;
 
 public class WolfBagFeatureRenderer extends FeatureRenderer<WolfEntityRenderState, WolfEntityModel> {
@@ -60,51 +62,55 @@ public class WolfBagFeatureRenderer extends FeatureRenderer<WolfEntityRenderStat
         }
 
 
-        boolean bl = false;
-        boolean bl2 = state.baby;
-        matrices.push();
-        float m;
-        if (bl2) {
-            m = 0.75F;
-            matrices.scale(0.75F, 0.75F, 0.75F);
-            matrices.translate(0.0F, 0.5F, 0.209375F);
-        }
+        final BakedModel bakedModel = state.getMainHandItemModel();
+        final ItemStack itemStack = state.getMainHandStack();
 
-        matrices.translate(wolfHead.pivotX / 16.0F, wolfHead.pivotY / 16.0F, wolfHead.pivotZ / 16.0F);
-        //m = wolfHead.getHeadRoll(h);
-        //m = foxEntity.getHeadRoll(h);
-        m = state.getBegAnimationProgress(partialTicks);
-        //if (state.isWet())
-        m = state.getShakeAnimationProgress(partialTicks, 0f) + state.getBegAnimationProgress(partialTicks);
-
-        matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(netHeadYaw));
-        matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(headPitch));
-        matrices.multiply(RotationAxis.POSITIVE_Z.rotation(m));
-        //matrices.multiply(RotationAxis.POSITIVE_Y.rotation(m));
-        //matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(m));
-        if (state.isBaby()) {
-            if (bl) {
-                matrices.translate(0.4F, 0.26F, 0.15F);
-            } else {
-                matrices.translate(0.06F, 0.26F, -0.5F);
+        if (bakedModel != null && !itemStack.isEmpty()) {
+            boolean bl = false;
+            boolean bl2 = state.baby;
+            matrices.push();
+            float m;
+            if (bl2) {
+                m = 0.75F;
+                matrices.scale(0.75F, 0.75F, 0.75F);
+                matrices.translate(0.0F, 0.5F, 0.209375F);
             }
-        } else if (bl) {
-            matrices.translate(0.46F, 0.26F, 0.22F);
-        } else {
-            //matrices.translate(0.06F, 0.83F, -0.4F);
-            matrices.translate(0.06F, 0.13F, -0.4F);
+
+            matrices.translate(wolfHead.pivotX / 16.0F, wolfHead.pivotY / 16.0F, wolfHead.pivotZ / 16.0F);
+            //m = wolfHead.getHeadRoll(h);
+            //m = foxEntity.getHeadRoll(h);
+            m = state.begAnimationProgress;
+            //if (state.isWet())
+            m = state.shakeProgress;
+
+            matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(limbAngle));
+            matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(limbDistance));
+            matrices.multiply(RotationAxis.POSITIVE_Z.rotation(m));
+            //matrices.multiply(RotationAxis.POSITIVE_Y.rotation(m));
+            //matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(m));
+            if (state.baby) {
+                if (bl) {
+                    matrices.translate(0.4F, 0.26F, 0.15F);
+                } else {
+                    matrices.translate(0.06F, 0.26F, -0.5F);
+                }
+            } else if (bl) {
+                matrices.translate(0.46F, 0.26F, 0.22F);
+            } else {
+                //matrices.translate(0.06F, 0.83F, -0.4F);
+                matrices.translate(0.06F, 0.13F, -0.4F);
+            }
+
+            //if (!state.isSitting())
+            matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(90.0F));
+            if (bl) {
+                matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(90.0F));
+            }
+
+            //ItemStack itemStack = state.getMainHandStack();
+
+            this.heldItemRenderer.renderItem(itemStack, ModelTransformationMode.GROUND, false, matrices, vertexConsumers, light, OverlayTexture.DEFAULT_UV, bakedModel);
+            matrices.pop();
         }
-
-        //if (!state.isSitting())
-        matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(90.0F));
-        if (bl) {
-            matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(90.0F));
-        }
-
-        ItemStack itemStack = state.getEquippedStack(EquipmentSlot.MAINHAND);
-        //itemStack = Items.COOKED_BEEF.getDefaultStack();
-
-        this.heldItemRenderer.renderItem(state, itemStack, ModelTransformationMode.GROUND, false, matrices, vertexConsumers, light);
-        matrices.pop();
     }
 }
